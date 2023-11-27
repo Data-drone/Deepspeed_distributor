381e315ab5893e6b9b564b17d20719ca13324274
Brian <bpl.law@gmail.com>
2023-11-27 12:23:13 +1100

    updated the download models

--- download_models.py
+++ download_models.py
@@ -1,18 +1,28 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC
-# MAGIC # Model Loading
-# MAGIC When testing out a lot of models we don't want to have to keep downloading all the time
+# MAGIC # Model Preloading
+# MAGIC When testing out a lot of models we don't want to have to keep downloading all the time so we will cache them on dbfs
+# MAGIC
+# MAGIC To download from Huggingface we need to have a profile and a token. See [Documentation](https://huggingface.co/docs/hub/security-tokens) for details
+# MAGIC 
+# MAGIC It is never advisable to store passwords and tokens in plain text. In databricks secrets are the way to go. \
+# MAGIC See: [AWS Secrets](https://docs.databricks.com/en/security/secrets/index.html) | (Azure Secrets)[https://learn.microsoft.com/en-au/azure/databricks/security/secrets/]
+# MAGIC 
+# MAGIC Having to setup Databricks cli locally and use terminal can be a pain so you can also use our Python SDK
+# MAGIC See our [Python SDK Secrets Examples](https://github.com/databricks/databricks-sdk-py/blob/main/examples/secrets/put_secret_secrets.py) \
+# MAGIC **NOTE** with SDK you need to have the newest sdk installed `%pip install -U databricks-sdk`
 
 # COMMAND ----------
 
 # DBTITLE 1,HF Credentials
-
 import huggingface_hub
-huggingface_key = dbutils.secrets.get(scope='brian-hf', key='hf-key')
+huggingface_key = dbutils.secrets.get(scope='bootcamp_training', key='hf-key')
 huggingface_hub.login(token=huggingface_key)
 
 # setup home variables so that we don't run out of cache
+# If we don't do this, huggingface can cache to a the root folder `/` which only has ~200GB
+# We will set a cache on local_disk0 which is configuratable attached storage
 import os
 os.environ['HF_HOME'] = '/local_disk0/hf'
 os.environ['HUGGING_FACE_HUB_TOKEN'] = huggingface_key
@@ -20,6 +30,7 @@ os.environ['HUGGING_FACE_HUB_TOKEN'] = huggingface_key
 # COMMAND ----------
 
 # setup model path
+# In an enterprise env you may setup a shared folder for all users but I will store in a user folder here
 username = spark.sql("SELECT current_user()").first()['current_user()']
 
 downloads_home = f'/home/{username}/hf_models'
